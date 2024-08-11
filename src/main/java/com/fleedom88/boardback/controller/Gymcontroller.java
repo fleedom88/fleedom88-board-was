3e5f3eb073b2e00ccd579ddeb643ed6d83c5ab55
FREEDOM <fleedom88>
2024-08-11 22:59:44 +0900

    gym controller, 상수 추가

---
+++ src/main/java/com/fleedom88/boardback/controller/Gymcontroller.java
@@ -0,0 +1,10 @@
+package com.fleedom88.boardback.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/gym")
+public class Gymcontroller {
+    
+}
